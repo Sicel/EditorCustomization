b40b50e5300e6377b6e5abcb3e8f02c06cd0a3b9
Cristian <cristiandc10@live.com>
2019-10-17 02:01:38 -0400

    Initial commit

---
+++ Assets/Scripts/Equipment.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+    [Header("Equipment Stats")]
+    [SerializeField]
+    protected int durability = 10;
+}
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Equipment
+{
+    [Header("Sword Stats")]
+    [SerializeField]
+    private int damage = 10;
+}
